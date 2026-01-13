e2d670607a9e22e5ab686a214e74c800b54ea01b
ValdeciNovak <valdecirnovak@gmail.com>
2026-01-13 20:34:45 -0300

    Referenciando os posts do usuário

--- src/main/java/com/valdeci/workshopmongo/config/Instantiation.java
+++ src/main/java/com/valdeci/workshopmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.valdeci.workshopmongo.config;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -44,6 +45,9 @@ public class Instantiation implements CommandLineRunner {
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		
+		userRepository.save(maria);
 	}
 
 }
